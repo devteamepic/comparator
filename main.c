2163d4f3e91f5cb56f73d040113c4f8c9fb13332
Markitanov Denis <dmarkitanov@gmail.com>
2020-02-11 13:27:56 +0700

    removed invalid data

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
-#include "custom.h"
+#include <stdlib.h>
+#include <string.h>
+
+const char* getField(char* line, int num) {
+  const char* tok;
+  for (tok = strtok(line, ","); tok && *tok; tok = strtok(NULL, ",\n")) {
+    if (!--num) {
+      return tok;
+    }
+  }
+
+  return NULL;
+}
+
+
+int compareArrays(int a[], int b[], int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    if (a[i] != b[i]) {
+      return 1;
+    }
+  }
+  
+  return 0;
+}
 
 int main(){
-    printf("hello world");
-    printf(MAX_ARRAY_LENGTH);
-    return 0;
+  FILE* stream = fopen("./data/data.csv", "r");
+
+  char line[1024];
+  while(fgets(line, 1024, stream)) {
+    char* tmp = strdup(line);
+    printf("Field 8 %s\n", getField(tmp, 8));
+
+    free(tmp);
+  }
+  return 0;
 }
