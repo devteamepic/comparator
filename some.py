24ec65b3ba8b51ef780023f8a50d856a95fc994f
Markitanov Denis <dmarkitanov@gmail.com>
2020-02-27 11:03:05 +0600

    major changes without the actual deletion in filesystem

--- some.py
+++ some.py
@@ -2,31 +2,74 @@ import csv
 import glob, os
 import numpy as np
 
-arr = []
-fileArr = []
-counter = 0
 
-with open('./data.csv') as dataFile:
-    csvReader = csv.reader(dataFile, delimiter = ";")
-    for row in csvReader:
-        arr.append(row)
+def match_and_delete(subjectArray, counterpartArray):
+    counter = 0
+    subjectHolder = subjectArray
+    counterpartHolder = counterpartArray
 
-del arr[0]
-os.chdir("./files")
-for file in glob.glob("*.pdf"):
-    fileArr.append(file)
+    if subjectArray.ndim > 1:
+        subjectHolder = subjectArray[:, 3]
+    else:
+        counterpartHolder = counterpartArray[:, 3]
 
-arr = np.array(arr)
-fileArr = np.array(fileArr)
+    for item in subjectHolder:
+        if item not in counterpartHolder:
+            print(item)
+            subjectArray = np.delete(subjectArray, counter, 0)
+            return False, subjectArray
+        counter = counter + 1
 
-fileArr = fileArr[fileArr.argsort(kind="heapsort")]
-arr = arr[arr[:,3].argsort(kind="heapsort")]
-for a in arr:
-    print(counter)
-    print("1" + fileArr[counter])
-    print("2" + a[3])
-#    if (fileArr[counter] == a[3]):
-#        print("number: " + str(counter) + " exists")
-#    else:
-#        print("WARNING, number: " + str(counter) + " DOES NOT EXIST, Name is: " + a[3])
-    counter = counter + 1
+    return True, subjectArray
+
+
+def read_data_and_reshape(arr, fileArr):
+   with open('./data.csv') as dataFile:
+        csvReader = csv.reader(dataFile, delimiter = ";")
+        for row in csvReader:
+            arr.append(row)
+    
+        del arr[0]
+        os.chdir("./files")
+        for file in glob.glob("*.pdf"):
+            fileArr.append(file)
+        
+        arr = np.array(arr)
+        fileArr = np.array(fileArr)
+        
+        fileArr = fileArr[fileArr.argsort(kind="heapsort")]
+        arr = arr[arr[:,3].argsort(kind="heapsort")]
+
+        return arr, fileArr
+
+
+def convert(arr, fileArr):
+    trigger = False
+
+    if (type(arr).__module__ != np.__name__):
+       arr, fileArr = read_data_and_reshape(arr, fileArr) 
+
+    trigger, arr = match_and_delete(arr, fileArr)
+
+    if not trigger:
+        return trigger, arr, fileArr
+
+    trigger, fileArr = match_and_delete(fileArr, arr)
+
+    return trigger, arr, fileArr
+
+
+def main():
+    arr = []
+    fileArr = []
+    trigger = False
+    
+    while not trigger:
+        trigger, arr, fileArr = convert(arr, fileArr)
+    
+    print(arr[:, 3].size)
+    print(fileArr.size)
+
+
+if __name__ == "__main__":
+    main()
